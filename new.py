76ebc36b572cb51a655bd2c01db0d876a907cc82
sweta jena <swetajena989@gmail.com>
2018-08-28 07:47:53 +0530

    Update new.py

--- new.py
+++ new.py
@@ -1,1 +1,48 @@
 
+import json
+import turtle
+import urllib.request
+import time
+'''url = 'http://api.open-notify.org/astros.json'
+response = urllib.request.urlopen(url)
+result = json.loads(response.read())
+print('people in space: ',result['number'])
+people = result['people']
+for p in people:
+ print(p['name'],'in',p['craft'])'''
+url = 'http://api.open-notify.org/iss-now.json'
+response = urllib.request.urlopen(url)
+result = json.loads(response.read())
+location = result['iss_position']
+lat = location['latitude']
+long = location['longitude']
+print('latitude: ',lat)
+print('long: ',long)
+screen = turtle.Screen()
+screen.setup(720,360)
+screen.setworldcoordinates(-180,-90,180,90)
+screen.bgpic('map.jpg')
+screen.register_shape('iss2.png')
+iss = turtle.Turtle()
+iss.shape('iss2.png')
+iss.setheading(90)
+
+iss.penup()
+iss.goto(long,lat)
+lat = 29.5502
+lon = -95.007
+location = turtle.Turtle()
+location.penup()
+location.color('yellow')
+location.goto(lon,lat)
+location.dot(5)
+location.hideturtle()
+
+url =  'http://api.open-notify.org/iss-pass.json'
+url = url + '?lon='+str(lon)+'&lat='+str(lat)
+response = urllib.request.urlopen(url)
+result = json.loads(response.read())
+over = result['response'][1]['risetime']
+#print(over)
+style = ('Arial', 6 ,'bold')
+location.write(time.ctime(over) , font=style)
